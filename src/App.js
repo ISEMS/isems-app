40ed806991fa95a155dfd3d2670290c8be0ff492
Knut Hühne <knut@k-nut.eu>
2021-04-01 19:12:47 +0200

    Show two columns for node details on larger screens

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import {fetchData} from "./api";
 import "./App.sass";
 import NodeDetails from "./NodeDetails";
 
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -17,9 +16,25 @@ class App extends Component {
       nodes: [],
       loadError: false,
       showsInactiveNodes: false,
+      width: 0,
     };
     this.setData = this.setData.bind(this);
     this.showInactiveNodes = this.showInactiveNodes.bind(this);
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    this.loadNodes(false)
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
   }
 
   setData(nodes) {
@@ -37,10 +52,6 @@ class App extends Component {
   showInactiveNodes() {
     this.loadNodes(true);
     this.setState({showsInactiveNodes: true})
-  }
-
-  componentDidMount() {
-    this.loadNodes(false)
   }
 
   render() {
@@ -63,8 +74,19 @@ class App extends Component {
                       showsInactiveNodes={this.state.showsInactiveNodes}
             />
           </Route>
-          <Route exact path="/details/:nodeId" component={NodeDetails} />
-          <Route path="/details/:nodeId/charts" component={NodeCharts} />
+            <Route path="/details/:nodeId">
+                  <div className="multiCol">
+                    {this.state.width > 700 &&
+                    <NodeList nodes={this.state.nodes}
+                              error={this.state.loadError}
+                              onLoadAllNodes={this.showInactiveNodes}
+                              showsInactiveNodes={this.state.showsInactiveNodes}
+                    /> }
+
+                    <Route exact path="/details/:nodeId" component={NodeDetails} />
+                    <Route path="/details/:nodeId/charts" component={NodeCharts} />
+                  </div>
+            </Route>
         </Switch>
         <FooterNavigation />
       </div>
