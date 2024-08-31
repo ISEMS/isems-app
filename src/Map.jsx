fecaeda347f193568d80250c50745ac39e5d1e46
Knut Hühne <knut@k-nut.eu>
2024-08-31 10:27:48 +0200

    Switch to vite

--- src/Map.jsx
+++ src/Map.jsx
@@ -3,43 +3,43 @@ import React from "react";
 import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 import L from "leaflet";
 import { getFormattedDate } from "./utils";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "leaflet/dist/leaflet.css";
-import {useTranslation} from "react-i18next";
+import { useTranslation } from "react-i18next";
 
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
-});
+// delete L.Icon.Default.prototype._getIconUrl;
+//
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+//   iconUrl: require("leaflet/dist/images/marker-icon.png"),
+//   shadowUrl: require("leaflet/dist/images/marker-shadow.png")
+// });
 
 export default function NodeMap({ nodes }) {
-  const {t} = useTranslation();
+  const { t } = useTranslation();
 
   if (!nodes.length) {
-    return <div>{t('globals.loading')}...</div>;
+    return <div>{t("globals.loading")}...</div>;
   }
 
   const markers = nodes
-    .filter(node => node.latitude && node.longitude)
-    .map(node => (
+    .filter((node) => node.latitude && node.longitude)
+    .map((node) => (
       <Marker position={[node.latitude, node.longitude]} key={node.nodeId}>
         <Popup>
           <div>
             <h1> {node.name}</h1>
             <div>
-              <span className="title">{t('globals.date')}:</span>
+              <span className="title">{t("globals.date")}:</span>
               {getFormattedDate(node.timestamp)}
             </div>
             <div>
-              <span className="title">{t('globals.batteryCharge')}:</span>
+              <span className="title">{t("globals.batteryCharge")}:</span>
               {node.batteryChargeEstimate}
             </div>
             <div>
-              <Link to={`/details/${node.nodeId}`}>{t('globals.details')}</Link>
+              <Link to={`/details/${node.nodeId}`}>{t("globals.details")}</Link>
             </div>
           </div>
         </Popup>
@@ -50,7 +50,7 @@ export default function NodeMap({ nodes }) {
     <Map center={[nodes[0].latitude, nodes[0].longitude]} zoom={13}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
       {markers}
     </Map>
